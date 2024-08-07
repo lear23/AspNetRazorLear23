3d93f9daee1ed808f04ecfa10c646aac863cd3da
lear23 <130910243+lear23@users.noreply.github.com>
2024-08-07 11:09:57 +0200

    fixade fält in i signup

---
+++ AspNetRazorLear23/Models/SignUpFormModel.cs
@@ -0,0 +1,36 @@
+﻿using Infrastructure.Helpers;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace AspNetRazorLear23.Models
+{
+    public class SignUpFormModel
+    {
+        [DisplayName("First name")]
+        [Required(ErrorMessage = "Invalid first name")]
+        public string FirstName { get; set; } = null!;
+
+        [DisplayName("Last name")]
+        [Required(ErrorMessage = "Invalid last name")]
+        public string LastName { get; set; } = null!;
+
+        [DisplayName("Email Address")]
+        [Required(ErrorMessage = "Invalid email address")]
+        public string Email { get; set; } = null!;
+
+        [DisplayName("Password")]
+        [Required(ErrorMessage = "Invalid password")]
+        public string Password { get; set; } = null!;
+
+
+        [DisplayName("Confirm password")]
+        [Required(ErrorMessage = "Must confirm password")]
+        [Compare(nameof(Password), ErrorMessage ="Password does not match")]
+        public string ConfirmPassword { get; set; } = null!;
+
+        [DisplayName("I agree to the Terms & Conditions")]
+        [CheckBoxRequired(ErrorMessage = "You must agree to the terms & conditions")]
+        public bool Terms {  get; set; }
+
+    }
+}
--- AspNetRazorLear23/Pages/SignUp.cshtml.cs
+++ AspNetRazorLear23/Pages/SignUp.cshtml.cs
@@ -1,12 +1,34 @@
+
+using AspNetRazorLear23.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+
 
 namespace AspNetRazorLear23.Pages
 {
     public class SignUpModel : PageModel
     {
+
+        [BindProperty]
+        public SignUpFormModel Form { get; set; } = new SignUpFormModel();
+
+
         public void OnGet()
         {
+           
         }
+
+        public IActionResult OnPost() 
+        {
+            if (!ModelState.IsValid)
+            { 
+                return Page();
+            }
+            return RedirectToPage("/index");
+        }
+       
+
     }
+
+    
 }
